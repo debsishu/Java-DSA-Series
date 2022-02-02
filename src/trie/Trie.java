0d1871c052111f1e5d6392e45ee3fdd86686e2de
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-02-02 22:34:59 +0530

    Trie Add

---
+++ src/trie/Trie.java
@@ -0,0 +1,69 @@
+package trie;
+
+public class Trie {
+
+	private static Node root;
+
+	public Trie() {
+		root = new Node();
+	}
+
+	void insert(String word) {
+		Node node = root;
+
+		for (int i = 0; i < word.length(); i++) {
+			if (!node.containsKey(word.charAt(i))) {
+				node.put(word.charAt(i), new Node());
+			}
+			node = node.get(word.charAt(i));
+		}
+		node.setEnd();
+	}
+
+	boolean search(String word) {
+		Node node = root;
+		for (int i = 0; i < word.length(); i++) {
+			if (!node.containsKey(word.charAt(i))) {
+				return false;
+			}
+			node = node.get(word.charAt(i));
+		}
+		return node.isEnd();
+	}
+
+	boolean startsWith(String prefix) {
+		Node node = root;
+		for (int i = 0; i < prefix.length(); i++) {
+			if (!node.containsKey(prefix.charAt(i))) {
+				return false;
+			}
+			node = node.get(prefix.charAt(i));
+		}
+		return true;
+	}
+
+	static class Node {
+		Node links[] = new Node[26];
+		boolean flag = false;
+
+		boolean containsKey(char ch) {
+			return (links[ch - 'a'] != null);
+		}
+
+		Node get(char ch) {
+			return links[ch - 'a'];
+		}
+
+		void put(char ch, Node node) {
+			links[ch - 'a'] = node;
+		}
+
+		void setEnd() {
+			flag = true;
+		}
+
+		boolean isEnd() {
+			return flag;
+		}
+	}
+}
